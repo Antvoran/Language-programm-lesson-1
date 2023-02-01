86abbee0833ab4786d20777716ce5a2a03f02692
Антон <Antvoran@yandex.by>
2023-02-02 01:01:21 +0300

    Добавили example005 привет Маша

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите имя");
+string username = Console.ReadLine();
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Здравствуй, Машенька!");
+}
+else 
+{
+    Console.Write("Здарова, ");
+    Console.WriteLine(username);
+}
